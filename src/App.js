da1e51af0ca8da49124379b713a9ec9d6bc14216
hashirahmed2982 <92557515+hashirahmed2982@users.noreply.github.com>
2023-08-25 18:29:57 +0300

    j

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import DynamicTable from "./dynamic-table/DynamicTable";
+import axios from "axios";
+import { Component } from "react";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  const [datas, setdatas] = useState([]);
+ 
+  
+  useEffect(() => {
+    axios.get("http://localhost:4000/user/users").then(({ data }) => {
+      setdatas(data);
+      console.log(datas);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }, []);
+  if(datas.length !== 0 ){
+    return (
+      console.log(datas.length),
+      <DynamicTable TableData={datas} />
+    );
+  }
+  else{
+    return(
+      "no data found"
+    );
+  }
+  
+  
+}
+export default App;
---
+++ src/dynamic-table/DynamicTable.js
@@ -0,0 +1,52 @@
+
+//import TableData from "./TableData";
+import React, { useState, useEffect } from "react";
+import { Table } from "react-bootstrap";
+function DynamicTable ({TableData}) {
+
+
+  
+// get table column
+ const column = Object.keys(TableData[0]);
+
+ // get table heading data
+ const ThData =()=>{
+    
+     return column.map((data)=>{
+         return <th  key={data}>{data.toUpperCase()}</th>
+     })
+ }
+
+// get table row data
+const tdData = () =>{
+  
+   
+     return TableData.map((data)=>{
+       return(
+           <tr >
+                {
+                   column.map((v)=>{
+                       return <td  >{data[v]}</td>
+                   })
+                }
+           </tr>
+       )
+     })
+}
+
+
+  return (
+    <div className="table-wrapper" >
+      <Table striped bordered  variant="dark" responsive >
+        <thead>
+         <tr >{ThData()}</tr>
+        </thead>
+        <tbody>
+        {tdData()}
+        </tbody>
+       </Table>
+       </div>
+       
+  )
+}
+export default DynamicTable;
